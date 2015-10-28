512ec9be511b62d6009819528dc6e8011bc7bd97
Feng Jia <jia.feng@rea-group.com>
2015-10-28 14:47:01 +0800

    Add proper access control headers

--- lib/api.rb
+++ lib/api.rb
@@ -5,6 +5,11 @@ module EStore
     format :json
     prefix :api
 
+    before do
+      header 'Access-Control-Allow-Origin', '*'
+      header 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Api-Key'
+    end
+
     mount Api::User
   end
 end
