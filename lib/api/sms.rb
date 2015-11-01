6fd58ee8f4a957768c1f57e9e819f12fc2f5c3f9
Feng Jia <jia.feng@rea-group.com>
2015-11-01 15:11:10 +0800

    prevent sms attack

--- lib/api/sms.rb
+++ lib/api/sms.rb
@@ -2,14 +2,28 @@ module EStore
   module Api
     class SMS < Grape::API
 
+      helpers do
+        def same_client_ip_within_60_seconds
+          EStore::SMS.count(:client_ip => request.ip, :created_at.gt => Time.now - 60.seconds) > 0
+        end
+
+        def same_phone_number_within_60_seconds
+          EStore::SMS.count(:phone => params[:phone], :created_at.gt => Time.now - 60.seconds) > 0
+        end
+      end
+
       resources :sms do
         desc 'Send SMS'
         params do
           requires :phone, type: String, desc: 'Phone number', regexp: /^1\d{10}$/
         end
         post do
-          EStore::SMS.create(client_ip: request.ip, phone: params[:phone])
-          body false
+          if same_client_ip_within_60_seconds or same_phone_number_within_60_seconds
+            error! '短信发送过于频繁，请稍后再试', 400
+          else
+            EStore::SMS.create(client_ip: request.ip, phone: params[:phone])
+            body false
+          end
         end
       end
 
--- spec/api/sms_spec.rb
+++ spec/api/sms_spec.rb
@@ -16,16 +16,16 @@ describe EStore::Api::SMS do
     end
 
     context 'valid phone number' do
-      let(:phone_number) { {phone: '13891438527'} }
+      let(:request_data) { {phone: '13891438527'} }
 
       it 'responds 204' do
-        post '/api/sms', phone_number
+        post '/api/sms', request_data
 
         expect(last_response.status).to eq(204)
       end
 
       it 'saves sms detail' do
-        post '/api/sms', phone_number
+        post '/api/sms', request_data
 
         sms = EStore::SMS.first(phone: '13891438527')
 
@@ -33,13 +33,67 @@ describe EStore::Api::SMS do
         expect(sms.client_ip).to eq '127.0.0.1'
         expect(sms.code).to eq '000123'
       end
+
+      context 'request from same client ip twice' do
+        before do
+          EStore::SMS.create({client_ip: '127.0.0.1', phone: '13888888888', created_at: previous_sms_time})
+        end
+
+        context 'within 60 seconds' do
+          let(:previous_sms_time) { Time.now }
+
+          it 'responds 400' do
+            post '/api/sms', request_data
+
+            expect(last_response.status).to eq(400)
+            expect(JSON.parse(last_response.body)['error']).to eq('短信发送过于频繁，请稍后再试')
+          end
+        end
+
+        context 'after 60 seconds' do
+          let(:previous_sms_time) { Time.now - 61.seconds }
+
+          it 'responds 204' do
+            post '/api/sms', request_data
+
+            expect(last_response.status).to eq(204)
+          end
+        end
+      end
+
+      context 'request to same target phone number twice' do
+        before do
+          EStore::SMS.create({client_ip: '8.8.8.8', phone: '13891438527', created_at: previous_sms_time})
+        end
+
+        context 'within 60 seconds' do
+          let(:previous_sms_time) { Time.now }
+
+          it 'responds 400' do
+            post '/api/sms', request_data
+
+            expect(last_response.status).to eq(400)
+            expect(JSON.parse(last_response.body)['error']).to eq('短信发送过于频繁，请稍后再试')
+          end
+        end
+
+        context 'after 60 seconds' do
+          let(:previous_sms_time) { Time.now - 61.seconds }
+
+          it 'responds 204' do
+            post '/api/sms', request_data
+
+            expect(last_response.status).to eq(204)
+          end
+        end
+      end
     end
 
     context 'invalid phone number' do
-      let(:phone_number) { {phone: 'abc'} }
+      let(:request_data) { {phone: 'abc'} }
 
       it 'responds 400' do
-        post '/api/sms', phone_number
+        post '/api/sms', request_data
 
         expect(last_response.status).to eq(400)
       end
